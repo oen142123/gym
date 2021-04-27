fadf11e2067366ed1c2aff88c87d6d7afa685f13
wani <oen142@naver.com>
2021-04-27 11:12:45 +0900

    Initial commit

---
+++ src/test/java/com/wani/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
